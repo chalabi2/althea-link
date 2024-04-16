823ad02a355af4ec1d003e567ef088d81fe84faf
Blaine <blaine.d.oneill@gmail.com>
2024-04-16 13:51:15 -0700

    fix type errors (added TS-IGNOREs to utils.ts)

--- components/althea_net/AltheaL.tsx
+++ components/althea_net/AltheaL.tsx
@@ -1,10 +1,21 @@
 // AltheaL SVG icon component
-export const AltheaL = ({ theme }) => {
+interface AltheaLProps {
+  theme?: string;
+}
+
+export const AltheaL = ({ theme }: AltheaLProps) => {
   return (
-    <svg id="althea-l" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15.18 26.02">
+    <svg
+      id="althea-l"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 15.18 26.02"
+    >
       <g id="group">
-        <polygon fill={theme == 'white' ? '#fff' : '#07f'} points="0 0 0 26.02 15.18 26.02 15.18 21.66 4.92 21.66 4.92 0 0 0"/>
+        <polygon
+          fill={theme == "white" ? "#fff" : "#07f"}
+          points="0 0 0 26.02 15.18 26.02 15.18 21.66 4.92 21.66 4.92 0 0 0"
+        />
       </g>
     </svg>
-  )
-}
+  );
+};
