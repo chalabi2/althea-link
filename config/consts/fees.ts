8c5d4b4615c9bf606e992b941dbc26832a12cd9d
Joseph Chalabi <100090645+chalabi2@users.noreply.github.com>
2024-05-03 19:23:43 -0700

    chore: disable migrator | adjust fees

--- config/consts/fees.ts
+++ config/consts/fees.ts
@@ -1,47 +1,47 @@
 import { Fee } from "@/transactions/interfaces";
 
 export const CONVERT_FEE: Fee = {
-  amount: "2300000000000000000",
+  amount: "80000000000000000",
   denom: "aalthea",
-  gas: "2300000",
+  gas: "800000",
 };
 
 export const IBC_FEE: Fee = {
-  amount: "3000000000000000000",
+  amount: "80000000000000000",
   denom: "aalthea",
-  gas: "3000000",
+  gas: "800000",
 };
 
 export const PUB_KEY_FEE: Fee = {
-  amount: "100000000000000000",
+  amount: "80000000000000000",
   denom: "aalthea",
-  gas: "3000000",
+  gas: "800000",
 };
 
 export const DELEGATE_FEE: Fee = {
-  amount: "1000000000000000000",
+  amount: "40000000000000000",
   denom: "aalthea",
-  gas: "3000000",
+  gas: "300000",
 };
 
 export const UNDELEGATE_FEE: Fee = {
-  amount: "1000000000000000000",
+  amount: "40000000000000000",
   denom: "aalthea",
-  gas: "3000000",
+  gas: "400000",
 };
 
 export const REDELEGATE_FEE: Fee = {
-  amount: "1000000000000000000",
+  amount: "40000000000000000",
   denom: "aalthea",
-  gas: "3000000",
+  gas: "400000",
 };
 export const CLAIM_STAKING_REWARD_FEE: Fee = {
-  amount: "1000000000000000000",
+  amount: "80000000000000000",
   denom: "aalthea",
-  gas: "3000000",
+  gas: "800000",
 };
 export const VOTING_FEE: Fee = {
-  amount: "1000000000000000000",
+  amount: "80000000000000000",
   denom: "aalthea",
-  gas: "3000000",
+  gas: "800000",
 };
